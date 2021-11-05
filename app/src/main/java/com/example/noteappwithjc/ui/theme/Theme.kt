046e24a6ac83746f60bea92bfc6137c2f4a36109
sandeep <sksandeep9215@gmail.com>
2021-11-05 07:53:56 +0530

    add dependencies and theme colors

---
+++ app/src/main/java/com/example/noteappwithjc/ui/theme/Theme.kt
@@ -0,0 +1,31 @@
+package com.example.noteappwithjc.ui.theme
+
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+private val DarkColorPalette = darkColors(
+    primary = Color.White,
+    background = DarkGray,
+    onBackground = Color.White,
+    surface = LightBlue,
+    onSurface = DarkGray
+)
+
+private val LightColorPalette = lightColors(
+    primary = Color.Black,
+    primaryVariant = DarkGray,
+    secondary = LightBlue
+)
+
+@Composable
+fun NoteAppWithJCTheme(darkTheme: Boolean = true, content: @Composable() () -> Unit) {
+    MaterialTheme(
+        colors = DarkColorPalette,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
